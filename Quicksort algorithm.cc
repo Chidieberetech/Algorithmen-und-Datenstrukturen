8c51d5f504e8cc0ff1a54115e13ef7aaeb07a2ed
Teroxlab <emmanuels820@gmail.com>
2023-05-02 21:57:16 +0200

    script that uses the algorithm with different inputs.

--- Quicksort algorithm.cc
+++ Quicksort algorithm.cc
@@ -1,14 +1,14 @@
-// Quick sort in C++
+// Quick-sorting in C++
 
 #include  "iostream"
 
 using namespace std;
 
-// script to interchange the element
+//*** interchanging the element
 void swap(int *g, int *h) 
 { int t = *g; *g = *h; *h = t; }
 
-// script to print the array
+//*** Output of  the array
 void printArray(int array[], int size) {
   int i;
   for (i = 0; i < size; i++)
@@ -16,62 +16,47 @@ void printArray(int array[], int size) {
   cout << endl;
 }
 
-int partition(int array[], int low, int high) {
-      
-  int i = low, j = high;
-  int pivot = array[high];
-  
-  // pointer for greater element
-  int i = (low - 1);
 
-  // traverse each element of the array
-  // compare them with the pivot
+int partition(int array[], int low, int high) {
+  int pivot = array[high];
+  int i = (low - 1);
   for (int j = low; j < high; j++) {
     if (array[j] <= pivot) {
-        
-      // if element smaller than pivot is found
-      // swap it with the greater element pointed by i
+
       i++;
       
-      // swap element at i with element at j
       swap(&array[i], &array[j]);
     }
   }
   
-  // swap pivot with the greater element at i
   swap(&array[i + 1], &array[high]);
   
-  // return the partition point
   return (i + 1);
 }
 
 void quickSort(int array[], int low, int high) {
   if (low < high) {
       
-    // find the pivot element such that
-    // elements smaller than pivot are on left of pivot
-    // elements greater than pivot are on righ of pivot
     int pi = partition(array, low, high);
 
-    // recursive call on the left of pivot
-    quickSort(array, low, pi - 1);
+        quickSort(array, low, pi - 1);
 
-    // recursive call on the right of pivot
+    
     quickSort(array, pi + 1, high);
   }
 }
 
 // main function/script that uses the algorithm with different inputs.
 int main() {
-  int data[] = {8, 7, 6, 1, 0, 9, 2};
+  int data[] = {9, 12, 11, 10, 14, 16, 13};
   int n = sizeof(data) / sizeof(data[0]);
   
-  cout << "Unsorted Array: \n";
+  cout << "This the Unsorted Array: \n";
   printArray(data, n);
   
-  // perform quicksort on data
+  // making quicksort on inputed data
   quickSort(data, 0, n - 1);
   
-  cout << "Sorted array in ascending order: \n";
+  cout << "Eureka the Sorted array: \n";
   printArray(data, n);
 }
